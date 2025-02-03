48823effc126d3aa126eb761a34a7a8e3ec2368b
Enrico Wegner <wegner1996@gmail.com>
2025-02-03 21:00:41 +0100

    extending dsge example

--- examples/matlab/dynare/smets_wouters.m
+++ examples/matlab/dynare/smets_wouters.m
@@ -16,7 +16,7 @@ obsVar = getObsVarIds(M_, options_, oo_, obsVar);
 
 order = defineOrder({"robs", "piexp", "dw", "dc", "dinve", "dy", "pinfobs"}, vars);
 
-% need to adjust the above function so it directly has this form
+% TODO: need to adjust the above function so it directly has this form
 % also note that returned coeffs are structural, we work with reduced form
 Phi0 = inv(A0);
 Sigma = Phi0 * Phi0';
@@ -32,9 +32,37 @@ end
 
 [B, Omega] = makeSystemsForm(Phi0, AsCellArray, PsisCellArray, Sigma, order, 1);
 
+% TODO: Should write a helper function for this
 shocks = dynareCellArray2Vec(M_.exo_names);
 ix_em = find(shocks == "em");
 shock_size = sqrt(M_.Sigma_e(ix_em, ix_em));
 
+% Checking for correctness by comparing to dynare output
 condTrue = Q('T');
 irfs = transmission(ix_em, B, Omega, condTrue, "BOmega") * shock_size;
+
+% creating the interest rate channel
+interest_channel = notThrough([1,2,3,4,7], {0:1, 0:1, 0:1, 0:1, 0:1}, order);
+interest_effect = transmission(ix_em, B, Omega, interest_channel, "BOmega", order) * shock_size;
+
+% The expectations channel says through expectations in at least one period
+% and not through the others. The OR cannot be straight forward handled so 
+% we use the trick of negating the statement:
+% ~(x1 | x2) = ~x1 & ~x2
+expectations_channel = ~notThrough(4, 0:1, order) & notThrough([1,2,3,7], {0:1, 0:1, 0:1, 0:1}, order);
+expectations_effect = transmission(ix_em, B, Omega, expectations_channel, "BOmega", order) * shock_size;
+% alternatively
+% this does not directly work because of other simplification
+% make things that should not be interpreted NaN
+% TODO: things should be NaN if they should not be interpreted.
+stringCondition = "(y_{4,0} | y_{4,1})";
+condition = makeConditionY(stringCondition, order);
+expectations_channel = condition & notThrough([1,2,3,7], {0:1, 0:1, 0:1, 0:1}, order);
+expectations_effect = transmission(ix_em, B, Omega, expectations_channel, "BOmega", order) * shock_size;
+
+% Similar problem here. The channel is technically the effect through any of 
+% the output or wage variables in at least one period. We cannot directly 
+% implement this, but we can again use the negation trick from above
+output_wage_channel = ~notThrough([1,2,3,7], {0:1, 0:1, 0:1, 0:1}, order);
+output_wage_effect = transmission(ix_em, B, Omega, output_wage_channel, "BOmega", order) * shock_size;
+
